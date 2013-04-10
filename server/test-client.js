33cb4f8d293a41a7643182e49d045ba431de59fd
Andrew Breja <asb@andrews.edu>
2013-04-10 15:54:10 -0400

    Command line parsing options for test-client

--- server/test-client.js
+++ server/test-client.js
@@ -14,3 +14,31 @@
 
 var commander = require('commander');
 var io = require('socket.io-client');
+
+
+// --------------------------------------------------------------
+// SETUP
+
+var socket = io.connect('localhost', { port: 5060 });
+
+
+// --------------------------------------------------------------
+// COMMAND LINE ARGUMENTS
+
+commander.version('0.1');
+commander.usage('[option] value');
+commander.option('-t --terms [terms]', 'Twitter search terms');
+commander.parse(process.argv);
+
+// For each search term
+for (term in commander.terms)
+{
+
+}
+
+socket.on('connect', function ()
+{
+	// do stuff
+});
+
+socket.emit()
